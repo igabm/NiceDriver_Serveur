043aa4deef725980ffddc5a8fb4962ce9aa26224
Nicolas <nicolasramond21@gmail.com>
2017-08-16 14:07:52 +0200

    Test integration des WS

--- src/PointWS.java
+++ src/PointWS.java
@@ -1,10 +1,11 @@
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-public class PointWS {
+public class PointWS extends Application{
 	   @GET 
 	   @Path("/points") 
 	   @Produces(MediaType.APPLICATION_JSON) 
